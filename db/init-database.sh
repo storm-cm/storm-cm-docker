7ef614e1ae9fa90d7d017902311fca714bb6a52b
Sam Morris <sam@blinkbox.com>
2015-03-23 13:18:03 +0000

    blah

--- db/init-database.sh
+++
@@ -1,7 +0,0 @@
-(
-    for db in scm amon rman metastore sentry nav navms; do
-        echo "CREATE ROLE $db LOGIN PASSWORD '$db'"
-        echo "CREATE DATABASE $db OWNER $db ENCODING 'UTF8'"
-    done
-    echo 'ALTER DATABASE metastore SET standard_conforming_strings = off'
-) | su postgres -c "PATH=$PATH postgres --single -E"
--- manager/entry.bash
+++ manager/entry.bash
@@ -4,8 +4,23 @@ set -eu
 
 chown cloudera-scm:cloudera-scm /opt/cloudera/parcel-repo
 
-source /etc/default/cloudera-scm-server
+(
+    count=10
+    until nc -z db $DB_PORT_5432_TCP_PORT; do
+        if [[ $((--count)) -lt 0 ]]; then
+            echo 'Database timeout' >&2
+            exit 1
+        fi
+        sleep 1
+    done
+)
 
+/usr/share/cmf/schema/scm_prepare_database.sh \
+    --host=db --port=$DB_PORT_5432_TCP_PORT  \
+    --user=postgres --password=$DB_ENV_POSTGRES_PASSWORD \
+    postgresql scm scm scm
+
+source /etc/default/cloudera-scm-server
 exec su \
     -s /bin/bash \
     -c 'exec /usr/sbin/cmf-server' \
